ee9ec5edaf977099f4b3b6690d71fe8f7cbafdab
DheerajKholia <dheeraj.kholia@yahoo.com>
2024-07-07 12:09:32 +0530

    Added project skeleton

---
+++ src/main/java/com/ecommerce/productcatalogservice/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.ecommerce.productcatalogservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "welcome";
+    }
+
+}
